2c1b05680fc1307218d43b9f3f4a7a36a24e287c
Abhishek kushwaha <86338762+Abbhiishek@users.noreply.github.com>
2021-09-26 18:34:12 +0530

    Update senpai_bot.py

--- senpai_bot.py
+++ senpai_bot.py
@@ -11,6 +11,9 @@ import json
 from discord.user import ClientUser
 import requests
 import asyncio
+from random import choice
+from discord.voice_client import VoiceClient
+import youtube_dl
 
 
 
@@ -59,72 +62,8 @@ async def on_ready():
     print(f">> Total  Active Servers : {len(client.guilds)}")
     print('>> Senpai is Onwork.')
     print(">> Data loaded.")
-                 
-
-
-
-
+               
 #do stuffs
-
-import discord
-from discord.ext import commands, tasks
-from discord.voice_client import VoiceClient
-import youtube_dl
-
-from random import choice
-
-youtube_dl.utils.bug_reports_message = lambda: ''
-
-ytdl_format_options = {
-    'format': 'bestaudio/best',
-    'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-    'restrictfilenames': True,
-    'noplaylist': True,
-    'nocheckcertificate': True,
-    'ignoreerrors': False,
-    'logtostderr': False,
-    'quiet': True,
-    'no_warnings': True,
-    'default_search': 'auto',
-    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
-}
-
-ffmpeg_options = {
-    'options': '-vn'
-}
-
-ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
-
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
-
-        self.data = data
-
-        self.title = data.get('title')
-        self.url = data.get('url')
-
-    @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
-        loop = loop or asyncio.get_event_loop()
-        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
-
-        if 'entries' in data:
-            # take first item from a playlist
-            data = data['entries'][0]
-
-        filename = data['url'] if stream else ytdl.prepare_filename(data)
-        return cls(discord.FFmpegPCMAudio(filename, **ffmpeg_options), data=data)
-
-
-client = commands.Bot(command_prefix='?')
-
-status = ['Jamming out to music!', 'Eating!', 'Sleeping!']
-
-@client.event
-async def on_ready():
-    change_status.start()
-    print('Bot is online!')
 
 @client.event
 async def on_member_join(member):
@@ -147,57 +86,22 @@ async def die(ctx):
 
 @client.command(name='credits', help='This command returns the credits')
 async def credits(ctx):
-    await ctx.send('Made by `RK Coding`')
-    await ctx.send('Thanks to `DiamondSlasher` for coming up with the idea')
-    await ctx.send('Thanks to `KingSticky` for helping with the `?die` and `?creditz` command')
+    await ctx.send('Made by `Abhishek Kushwaha`')
+    await ctx.send('Thanks to `Abhishek` for coming up with the idea')
 
 @client.command(name='creditz', help='This command returns the TRUE credits')
 async def creditz(ctx):
     await ctx.send('**No one but me, lozer!**')
 
-@client.command(name='play', help='This command plays music')
-async def play(ctx, url):
-    if not ctx.message.author.voice:
-        await ctx.send("You are not connected to a voice channel")
-        return
-
-    else:
-        channel = ctx.message.author.voice.channel
-
-    await channel.connect()
-
-    server = ctx.message.guild
-    voice_channel = server.voice_client
-
-    async with ctx.typing():
-        player = await YTDLSource.from_url(url, loop=client.loop)
-        voice_channel.play(player, after=lambda e: print('Player error: %s' % e) if e else None)
-
-    await ctx.send('**Now playing:** {}'.format(player.title))
-
-@client.command(name='stop', help='This command stops the music and makes the bot leave the voice channel')
-async def stop(ctx):
-    voice_client = ctx.message.guild.voice_client
-    await voice_client.disconnect()
 
 @tasks.loop(seconds=20)
 async def change_status():
     await client.change_presence(activity=discord.Game(choice(status)))
 
+    
+switchpresence.start()    
 client.run('token')
-
-
-
-
-
-
-
-
 #starting the loop for the switch_presence
-
-
-switchpresence.start()
-
 #rumming the client in the server
 
-client.run('ODg4NDE0MDM2NjYyODMzMTY0.YUSWDA.c_fqaRhhwVgsN79QYfqezlZGFAA')
+
