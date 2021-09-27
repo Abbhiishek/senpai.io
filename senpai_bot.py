905b592ed79fba7b60fb127bf7680675bbc6e8d3
Abhishek kushwaha <86338762+Abbhiishek@users.noreply.github.com>
2021-09-27 15:08:02 +0530

    Add files via upload

--- senpai_bot.py
+++ senpai_bot.py
@@ -4,6 +4,7 @@ from discord.errors import ClientException
 from discord.ext import commands
 from discord.ext import tasks
 import os
+from decouple import config
 from discord.enums import UserFlags
 from discord.flags import Intents
 import random
@@ -24,8 +25,8 @@ intents = discord.Intents.default()
 intents.members = True
 
 #senpai variables
-senpai_id = 888414036662833164
-client = commands.Bot(command_prefix=commands.when_mentioned_or(  'S.', 's.'), case_insensitive=True, intents=intents)
+senpai_id = 88841403666283316
+client = commands.Bot(command_prefix=commands.when_mentioned_or( '.', 'S.', 's.', 'senpai ','Senpai '), case_insensitive=True, intents=intents)
 client.remove_command("help")
 
 print(">>>> The Master Is Logging To The Server... \n >>>Please wait for the connections to stablish...////////")
@@ -38,7 +39,7 @@ def load_cogs():
 
 @client.command()
 @commands.is_owner()
-async def reload(ctx):
+async def r(ctx):
   for file in os.listdir("./cogs"):
     if file.endswith(".py") and not file.startswith("_"):
       client.reload_extension(f"cogs.{file[:-3]}")
@@ -65,40 +66,16 @@ async def on_ready():
                
 #do stuffs
 
-@client.event
-async def on_member_join(member):
-    channel = discord.utils.get(member.guild.channels, name='general')
-    await channel.send(f'Welcome {member.mention}!  Ready to jam out? See `?help` command for details!')
 
-@client.command(name='ping', help='This command returns the latency')
-async def ping(ctx):
-    await ctx.send(f'**Pong!** Latency: {round(client.latency * 1000)}ms')
 
-@client.command(name='hello', help='This command returns a random welcome message')
-async def hello(ctx):
-    responses = ['***grumble*** Why did you wake me up?', 'Top of the morning to you lad!', 'Hello, how are you?', 'Hi', '**Wasssuup!**']
-    await ctx.send(choice(responses))
-
-@client.command(name='die', help='This command returns a random last words')
-async def die(ctx):
-    responses = ['why have you brought my short life to an end', 'i could have done so much more', 'i have a family, kill them instead']
-    await ctx.send(choice(responses))
-
-@client.command(name='credits', help='This command returns the credits')
-async def credits(ctx):
-    await ctx.send('Made by `Abhishek Kushwaha`')
-    await ctx.send('Thanks to `Abhishek` for coming up with the idea')
-
-@client.command(name='creditz', help='This command returns the TRUE credits')
-async def creditz(ctx):
-    await ctx.send('**No one but me, lozer!**')
 
 
 
 
     
-switchpresence.start()    
-client.run('token')
+token = config("TOKEN")
+switchpresence.start()
+client.run(token)
 #starting the loop for the switch_presence
 #rumming the client in the server
 
