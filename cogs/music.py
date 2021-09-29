b885823c1598a578b03667e29c724935d9e9721c
Abhishek kushwaha <86338762+Abbhiishek@users.noreply.github.com>
2021-09-29 10:06:31 +0530

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -38,7 +38,7 @@ class Music(commands.Cog):
             info = ydl.extract_info(url , download=False)
             url2 = info['formats'][0]['url']
             source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
-        vc.play(source)
+            vc.play(source)
 
 
     @commands.command()
