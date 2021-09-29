9363ed4a4753cd157e3b2fc38583f0329662dac1
Abhishek kushwaha <86338762+Abbhiishek@users.noreply.github.com>
2021-09-29 13:31:03 +0530

    Update music.py

--- cogs/music.py
+++ cogs/music.py
@@ -31,18 +31,9 @@ class Music(commands.Cog):
         
         FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5','options':'-vn'}
         ydl_opts = {
-            'format': 'bestaudio/best',
-             'outtmpl': '%(extractor)s-%(id)s-%(title)s.%(ext)s',
-              'restrictfilenames': True,
-              'noplaylist': True,
-              'nocheckcertificate': True,
-              'ignoreerrors': False,
-                 'logtostderr': False,
-                'quiet': True,
-               'no_warnings': True,
-                'default_search': 'auto',
-               'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes}
-        }
+            'format': 'bestaudio/best'
+                'default_search': 'auto'
+              }
         vc = ctx.voice_client
 
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
