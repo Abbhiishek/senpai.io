2cd1c6674b0855c704e1bc947cb0f46244e2bfc8
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2021-11-19 10:46:18 +0530

    spelling mistakes

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -28,7 +28,7 @@ class MOD(commands.Cog):
     
     # COMMANDS
     @commands.command()
-    @commands.has_permissions(ban_member= True)
+    @commands.has_permissions(ban_members= True)
     async def addbanwords(self,ctx,msg):
         file=open(r"./banned_words.txt","rb+")
         if msg in file :
@@ -40,7 +40,7 @@ class MOD(commands.Cog):
             await ctx.send(msg+ "added to the banned words")
 
     @commands.command()
-    @commands.has_permissions(ban_member= True)
+    @commands.has_permissions(ban_members= True)
     async def ban(self,ctx , user : discord.user= None, reason= None):
         if user == None or user == ctx.message.author:
             em = discord.Embed(title="Ban", description ='You cannot  BAN  yourself >/',colour=discord.Color.red())
@@ -52,7 +52,7 @@ class MOD(commands.Cog):
         em1 = discord.Embed(title="Ban", description =f'{user} has been banned >/ from {ctx.guild.name} because of : {reason}',colour=discord.Color.red())
         await ctx.send(embed=em1)
     @commands.command()
-    @commands.has_permissions(ban_member= True)
+    @commands.has_permissions(ban_members= True)
     async def unban(self,ctx , * , member):
         banned_user = await ctx.guild.bans()
         member_name ,member_disc = member.spilt('#')
