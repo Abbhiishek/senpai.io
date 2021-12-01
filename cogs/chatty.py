1917cea24e44c46c373254f1bc2cb15ee402fa8c
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2021-12-01 17:05:52 +0530

    bfgj

--- cogs/chatty.py
+++ cogs/chatty.py
@@ -16,7 +16,7 @@ class chatty(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self , msg,):
-            if msg.author != self.user:
+            if msg.author != self.client:
                 
                 querystring = {"bid":"178","key":"sX5A2PcYZbsN5EY6","uid":"mashape","msg":f"{msg}"}
                 response = requests.request("GET", url, headers=headers, params=querystring)
